7c652cbebb664cb529f1011199749ab1bac882f5
Kevin Z <zkw917@gmail.com>
2021-06-10 15:51:18 -0400

    Rebase

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,87 +1,87 @@
-const remarkImportPartial = require('remark-import-partial');
+const remarkImportPartial = require("remark-import-partial");
 
 module.exports = {
-  title: 'Spruce Developer Portal',
-  tagline: '$sudo grok spruce --asap',
-  url: 'https://spruceid.dev',
-  baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'assets/logo.png',
-  organizationName: 'spruceid', // Usually your GitHub org/user name.
-  projectName: 'spruceid.dev', // Usually your repo name.
+  title: "Spruce Developer Portal",
+  tagline: "$sudo grok spruce --asap",
+  url: "https://spruceid.dev",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "assets/logo.png",
+  organizationName: "spruceid", // Usually your GitHub org/user name.
+  projectName: "spruceid.dev", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'Spruce',
+      title: "Spruce",
       logo: {
-        alt: 'Spruce Site Logo',
+        alt: "Spruce Site Logo",
         href: "https://spruceid.com/",
-        src: 'assets/logo.png',
+        src: "assets/logo.png",
       },
       items: [
         {
-          label: '.dev Portal',
-          to: '/docs/',
+          label: ".dev Portal",
+          to: "/docs/",
           useBaseUrl: "/docs/",
-          position: 'left',
+          position: "left",
         },
         {
-          to: '/docs/didkit/', 
-          label: 'DIDKit',
+          to: "/docs/didkit/",
+          label: "DIDKit",
           useBaseUrl: "/didkit/",
-          position: 'left'
+          position: "left",
         },
         {
-          to: '/docs/credible', 
-          label: 'Credible',
+          to: "/docs/credible",
+          label: "Credible",
           useBaseUrl: "/credible/",
-          position: 'left'
+          position: "left",
         },
         {
-          href: 'https://sprucesystems.medium.com/',
-          label: 'Blog',
+          href: "https://sprucesystems.medium.com/",
+          label: "Blog",
           useBaseUrl: "/blog/",
-          position: 'left'
+          position: "left",
         },
         {
-          href: 'https://github.com/spruceid/',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/spruceid/",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
           items: [
             {
-              label: 'SpruceID.com',
-              to: 'https://spruceid.com',
+              label: "SpruceID.com",
+              to: "https://spruceid.com",
             },
           ],
         },
         {
           items: [
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/sprucesystems',
+              label: "Twitter",
+              href: "https://twitter.com/sprucesystems",
             },
           ],
         },
         {
           items: [
             {
-              label: 'LinkedIn',
-              href: 'https://www.linkedin.com/company/sprucesystemsinc/',
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/company/sprucesystemsinc/",
             },
           ],
         },
         {
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/spruceid/',
+              label: "GitHub",
+              href: "https://github.com/spruceid/",
             },
           ],
         },
@@ -91,25 +91,23 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/spruceid/',
+          editUrl: "https://github.com/spruceid/",
           remarkPlugins: [remarkImportPartial],
         },
         blog: {
-          blogSidebarTitle: 'Titles',
-          blogSidebarCount: 'ALL',  
+          blogSidebarTitle: "Titles",
+          blogSidebarCount: "ALL",
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/spruceid/spruceid.dev/',
+          editUrl: "https://github.com/spruceid/spruceid.dev/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
